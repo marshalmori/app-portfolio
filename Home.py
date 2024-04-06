e12eedc93e98a0fbd3b116c9f232046ae32c4766
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-04-06 14:28:03 -0300

    Initial commit

---
+++ Home.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout='wide')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('images/photo.png')
+
+with col2:
+    st.title('Ardit Sulce')
+    content = """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a odio ultrices, 
+    dignissim lacus ac, placerat erat. Praesent id purus ac augue aliquam egestas. 
+    Aliquam ullamcorper ligula dignissim euismod hendrerit. Fusce vel ante ac ex 
+    elementum faucibus et et quam. Vivamus bibendum at sapien a sollicitudin. 
+    Quisque tincidunt ligula at posuere convallis. 
+    """
+    st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv('data.csv', sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Source Code]({row['url']})")
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Source Code]({row['url']})")
+
